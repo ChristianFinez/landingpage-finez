d4a5d995be5b4d526e3f64bfa81e1b6f4ec5a0f1
ChristianFinez <101962659+ChristianFinez@users.noreply.github.com>
2022-05-30 09:29:38 +0800

    Update About.js

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import aboutml from './aboutml.png'
 const About = () => {
   return (
     
     <div className="container">
-      <img src='image/aboutml.png' height={450} width={1100} alt=""/>
+      <img src={aboutml} height={450} width={1100} style={{width:"100%"}} style2={{height:"100%"}} alt=""/>
       <h4 className="text-center" style={{paddingTop: "5%"}}>
       Join your friends in Mobile Legends: Bang Bang, the brand new 5v5 MOBA showdown, and fight against real players! Choose your favorite heroes and build the perfect team with your comrades-in-arms! 10-second matchmaking, 10-minute battles. Laning, jungling, pushing, and teamfighting, all the fun of PC MOBA and action games in the palm of your hand! Feed your eSports spirit!
 
